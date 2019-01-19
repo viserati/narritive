bfeff52cf1ab362ad378fdc204d0e90a5abde8eb
Keith Turner <turner@keithturner.com>
2019-01-19 02:22:32 +0000

    Added additional remarks

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
+
+Additional remarks:
+
 """
 
 import os
